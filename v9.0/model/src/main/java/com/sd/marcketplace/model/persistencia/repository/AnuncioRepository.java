56d4af1221daa1eea86ce84f72f4c9b087354dd8
thalesHDL <thaleslima.1501@gmail.com>
2019-06-29 12:00:58 -0300

    final final ruim

--- v9.0/model/src/main/java/com/sd/marcketplace/model/persistencia/repository/AnuncioRepository.java
+++ v9.0/model/src/main/java/com/sd/marcketplace/model/persistencia/repository/AnuncioRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.sd.marcketplace.model.persistencia.table.TableAnuncio;
-import org.springframework.data.repository.query.Param;
 
 public interface AnuncioRepository extends JpaRepository<TableAnuncio, Long>{
 	
