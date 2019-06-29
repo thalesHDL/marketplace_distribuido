266a3c6687fb9c4e66ca7d55a862b2d732b193fb
igorwdutra <52335408+igorwdutra@users.noreply.github.com>
2019-06-29 02:54:31 -0300

    adsadsaadsd
    dsadsadassadsa

--- v9.0/model/src/main/java/com/sd/marcketplace/model/persistencia/repository/ComentariosRepository.java
+++ v9.0/model/src/main/java/com/sd/marcketplace/model/persistencia/repository/ComentariosRepository.java
@@ -1,9 +1,15 @@
 package com.sd.marcketplace.model.persistencia.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sd.marcketplace.model.persistencia.table.TableComentario;
 
 public interface ComentariosRepository extends JpaRepository<TableComentario, Long> {
-
+	@Query(value = "SELECT * FROM comentario WHERE id = ':id' ",nativeQuery=true)
+	public List<TableComentario> getComentariosByIdAnuncio( @Param("id") Long id);
+	
 }
